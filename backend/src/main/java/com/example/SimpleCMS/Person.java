13398774152e5027acba08ebf553599eab16209e
kushiji <kushiji@protonmail.com>
2021-06-07 15:16:14 +0200

    MySQL connection config updated

---
+++ backend/src/main/java/com/example/SimpleCMS/Person.java
@@ -0,0 +1,47 @@
+package com.example.SimpleCMS;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="PERSON")
+public class Person {
+    private int id;
+
+    private String name;
+
+    private String country;
+
+    @Id
+    @Column(name="id", nullable = false, unique = true)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name="country")
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString(){
+        return "id="+id+", name="+name+", country="+country;
+    }
+}
